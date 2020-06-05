b719cd1a34a34d34c511f0381fbf06367a85fc62
nguyenhuan123 <59545418+nguyenhuan123@users.noreply.github.com>
2020-06-05 11:21:23 +0700

    Add files via upload

---
+++ cloudapp-master/index.php
@@ -0,0 +1,230 @@
+<html lang="en">
+<head>
+<title>Pc Accessories Shopping Mall</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="myweb.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<div class="header">
+  <h1>PC Accessories Shopping Mall</h1>
+  <p>A <b>Shopping Mall</b> website created by <a href="http://facebook.com/">me</a>.</p>
+  <img src="img/scroll1.png" width=10%>
+</div>
+
+<div class="navbar">
+<a href="index.php">Home</a>
+  <a href="new.php">New Procduct</a>
+  <a href="s2.php">Second Hand</a>
+  <button class="right" onclick="window.location.href='register.html'"><b>Register Or Login</b></button>
+  <button class="right" onclick="window.location.href='cart.html'"><b>Cart</b></button>
+</div>
+</head>
+<body>
+<video width="1519" controls autoplay>
+<source src="img/intro.ogg" type="video/ogg">
+<source src="img/intro.mp4" type="video/mp4">
+</video>
+<div class="row">
+  <div class="side">
+    <h2>Best Seller</h2>
+    <h5>PC Case:</h5>
+	<div class="fake2">
+    <img src="img/img3.jpg" alt="img" style="width:100%">
+	<p>Name: Eagle B32 Case</p>
+	<p>Type: Case PC</p>
+	<p>Make your PC Cooler</p>
+	<h5>Price: 2,400,000</h5>
+	<button class="btn add" onclick="window.location.href=''"><i class="fa fa-cart-plus icon"></i>Add to Cart</button>
+	</div>
+    <p>This case is the best seller in out shop</p>
+    <h3>Good Deals</h3>
+	<div class="fake2">
+    <img src="img/img1.jpg" alt="img" style="width:100%">
+	<p>Name: PC Gaming TY1</p>
+	<p>Type: Gaming PC</p>
+	<p>With Best VGA</p>
+	<h5>Price: 54,000,000</h5>
+	<button class="btn add" onclick="window.location.href=''"><i class="fa fa-cart-plus icon"></i>Add to Cart</button>
+	</div>
+	<br>
+	<div class="fake2">
+    <img src="img/img2.jpg" alt="img" style="width:100%">
+	<p>Name: PC Render TP5</p>
+	<p>Type: Rendering PC</p>
+	<p>Best choice for designer</p>
+	<h5>Price: 40,000,000</h5>
+	<button class="btn add" onclick="window.location.href=''"><i class="fa fa-cart-plus icon"></i>Add to Cart</button>
+	</div>
+	<br>
+	<div class="fake2">
+    <img src="img/img4.jpg" alt="img" style="width:100%">
+	<p>Name: PC Gaming TOG1842</p>
+	<p>Type: PC Gaming</p>
+	<p>Have fake drive wheel to play game easier</p>
+	<h5>Price: 30,000,000</h5>
+	<button class="btn add" onclick="window.location.href=''"><i class="fa fa-cart-plus icon"></i>Add to Cart</button>
+	</div>
+  </div>
+  <div class="main">
+    <h2>New RAM</h2>
+    <h5>New RAM selection for you!</h5>
+    <div class="fakeimg">	
+	<div class="rowside">
+  <div class="columnside">
+	<div class="fake2">
+    <img src="img/RAM/R1.jpg" alt="ram" style="width:100%">
+	<p>Name: RAM RT2 Ultimate</p>
+	<p>Producer: Team Group</p>
+	<p>Capacity: 2GB</p>
+	<h5>Price: 400,	000</h5>
+	<button class="btn add" onclick="window.location.href=''"><i class="fa fa-cart-plus icon"></i>Add to Cart</button>
+	</div>
+  </div>
+  <div class="columnside">
+	<div class="fake2">
+    <img src="img/RAM/R2.jpg" alt="ram" style="width:100%">
+	<p>Name:RAM RT4 SSUL</p>
+	<p>Producer: SAMSUNG CORP</p>
+	<p>Capacity: 4GB</p>
+	<h5>Price: 700,000</h5>
+	<button class="btn add" onclick="window.location.href=''"><i class="fa fa-cart-plus icon"></i>Add to Cart</button>
+	</div>
+	</div>
+ <div class="columnside">
+	<div class="fake2">
+    <img src="img/RAM/R3.jpg" alt="ram" style="width:100%">
+	<p>Name: KINGTON DDR4 RAM QB1</p>
+	<p>Producer: KINGTON COMPANY</p>
+	<p>Capacity: 8GB</p>
+	<h5>Price: 1,200,000</h5>
+	<button class="btn add" onclick="window.location.href=''"><i class="fa fa-cart-plus icon"></i>Add to Cart</button>
+	</div>
+  </div>
+</div>
+	</div>
+    <br>
+    <h2>Best Product</h2>
+    <h5>Best User Feedback</h5>
+    <div class="fakeimg">
+	<div class="rowside">
+  <div class="columnside">
+	<div class="fake2">
+    <img src="img/laptop1.jpg" alt="prd" style="width:100%">
+	<p>Name: Asus ROG Gaming Laptop</p>
+	<p>Producer: Asus</p>
+	<p>Type: Laptop Gaming</p>
+	<h5>Price: 24,000,000</h5>
+	<button class="btn add" onclick="window.location.href=''"><i class="fa fa-cart-plus icon"></i>Add to Cart</button>
+	</div>
+  </div>
+  <div class="columnside">
+	<div class="fake2">
+    <img src="img/mb1.jpg" alt="prd" style="width:100%">
+	<p>Name: Mainboard Asrock TD012</p>
+	<p>Producer: Asrock</p>
+	<p>Type: Mainboard</p>
+	<h5>Price: 2,000,000</h5>
+	<button class="btn add" onclick="window.location.href=''"><i class="fa fa-cart-plus icon"></i>Add to Cart</button>
+	</div>
+	</div>
+ <div class="columnside">
+	<div class="fake2">
+    <img src="img/m1.jpg" alt="prd" style="width:100%">
+	<p>Name: Fuhlen Gaming Gear Mouse</p>
+	<p>Producer: Fuhlen</p>
+	<p>Type: Gaming Gear</p>
+	<h5>Price: 500,000</h5>
+	<button class="btn add" onclick="window.location.href=''"><i class="fa fa-cart-plus icon"></i>Add to Cart</button>
+	</div>
+	</div>
+  </div>
+</div><br>
+
+<div class="ctn">
+  <form action="action_page.php">
+    <h1>Feedback:</h1>
+	<div class="ctn">
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+	</div>
+  </form>
+  </div>
+
+Sponsored by
+<header style="background-color:white">
+<marquee><img src="img/scroll1.png" width=30%>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img src="img/scroll2.jpg" width=30%></marquee>
+</header>
+<div class="footer">
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="img/img_avatar.png" alt="avatar" class="avatar" style="width:100%">
+      <div class="container">
+        <h2>Truong Vi Kiet</h2>
+        <p class="title">Creator</p>
+        <p>Demo for lab submission</p>
+        <p>kiettvgcd191129@fpt.edu.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Our shop in Vietnam</h2>
+        <p class="title">Branches:</p>
+        <a class="ta" href="#">Hà Nội>></a>
+		124 Hoang Hoa Tham, Cau Giay, Ha Noi.
+		<br><br>
+		<a  class="ta" href="#">Đà Nẵng>></a>
+		324 Phan Chau Trinh, Hai Chau, Da Nang
+		<br><br>
+		<a class="ta" href="#">Sài Gòn>></a>
+		130 Le Thanh Tong, Binh Thanh, Sai Gon
+        <br><br>
+		<a class="ta" href="#">Cần Thơ>></a>
+		2nd Floor, Complex Building, Ngo Van, Can Tho
+		<style>
+		.ta{
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 20px;
+  text-decoration: none;
+}
+		</style>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <a href="http://facebook.com" class="fa fa-facebook"></a>Our Facebook<br>
+	  <a href="http://twitter.com" class="fa fa-twitter"></a>Our Twitter<br>
+	  <a href="http://mail.google.com" class="fa fa-google"></a>Our Gmail<br>
+	  <a href="http://reddit.com" class="fa fa-reddit"></a>Our Reddit<br>
+	  <b style="color:black">Phone Number:</b> it will be updated late!
+    </div>
+  </div>
+</div>
+	<h3 align="center">© Copyright 2019. Assignment 2: Create a shopping mall Website</h3>
+</div>
+</div>
+</div>	
+</body>
+</html>
